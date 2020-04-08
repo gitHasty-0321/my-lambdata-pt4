d52398f8785414c778653778dd7f56c19f489e49
steven hastings <steve.hastings0321@gmail.com>
2020-04-07 22:54:07 -0400

    requirements.txt file added

---
+++ my_mod.py
@@ -0,0 +1,9 @@
+
+# my_lambdata/my_mod.py
+
+
+
+def enlarge(n):
+    return n * 100
+
+    
--- my_script.py
+++ my_script.py
@@ -1,4 +1,8 @@
 
+import pandas as pd
 
 
 print("HAPPY TUESDAY NIGHT.")
+
+df = pd.DataFrame({"x":[1,2,3], "y":[4,5,6]})
+print(df.head())
